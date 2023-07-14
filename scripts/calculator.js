98b34416c83d9a872ad19d7e02ce2b9026144833
emiraelDev <emmzex19@gmail.com>
2023-07-14 18:47:46 +0100

    Added some buttons and improved code

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,7 @@
 
 let calArr = [
-  'AC',' ** ', ' % ', ' / ', 7, 8, 9, ' * ', 
-  4, 5, 6, ' - ', 1, 2, 3, ' + ', 0, '.', '='
+  'AC',' ** ', 'round', ' / ', 7, 8, 9, ' * ', 
+  4, 5, 6, ' - ', 1, 2, 3, ' + ', 0, '.', '=', 'DEL'
 ];
 
 let calHtml = ''
@@ -10,7 +10,7 @@ calArr.forEach((calValue) => {
   if (calValue === ' / ' || calValue === ' * ' || calValue === ' - ' || calValue === ' + ') {
     calHtml += `<button id = "evalBtn" class = "evalBtn">${calValue}</button>`
     
-  } else if (calValue === 'AC' || calValue === ' ** ' || calValue === ' % ') {
+  } else if (calValue === 'AC' || calValue === ' ** ' || calValue === 'round') {
     calHtml += `<button id ="upBtn" class = "upBtn">${calValue}</button>`
   } else {
   calHtml += `<button id = "valueBtn" class = "valueBtn">${calValue}</button>`
@@ -30,6 +30,14 @@ function calculation (val) {
     calculated = 0;
   } else if (val === '=') {
      calculated = eval(calculated)
+  } else if (val === 'round') {
+    calculated = Math.ceil(eval(calculated))
+  } else if (val === 'DEL') {
+    calculated = calculated.slice(0, -1)
+    if (calculated === '') {
+      calculated = 0
+    }
+    console.log(calculated)
   } else {
   calculated += val;
   }
@@ -42,7 +50,7 @@ function calculation (val) {
 //calculation ()
 
 
-document.querySelectorAll('.evalBtn').forEach ((buttons) => {
+/*document.querySelectorAll('.evalBtn').forEach ((buttons) => {
   buttons.addEventListener ('click', () => {
     calculation (buttons.innerHTML);
     //console.log(buttons.innerHTML)
@@ -54,9 +62,9 @@ document.querySelectorAll('.upBtn').forEach ((buttons) => {
     calculation (buttons.innerHTML);
     //console.log(buttons.innerHTML)
   })
-})
+})*/
 
-document.querySelectorAll('.valueBtn').forEach ((buttons) => {
+document.querySelectorAll('button').forEach ((buttons) => {
   buttons.addEventListener ('click', () => {
     calculation (buttons.innerHTML);
     //console.log(buttons.innerHTML)
