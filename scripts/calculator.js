b9eed104d958da004c0ec467c4d6c46e3114df71
emiraelDev <emmzex19@gmail.com>
2023-07-14 12:30:53 +0100

    button grouping for easy styling

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,16 @@
-let calArr = ['AC','**', '%', '/']
+let calArr = [
+  'AC','**', '%', '/', 7, 8, 9, 'x', 
+  4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='
+]
 let calHtml = ''
 calArr.forEach((calValue) => {
-  calHtml += `<button>${calValue}</button>`
+  
+  if (calValue === '/' || calValue === 'x' || calValue === '-' || calValue === '+') {
+    calHtml += `<button class = "evalBtn">${calValue}</button>`
+  } else if (calValue === 'AC' || calValue === '**' || calValue === '%') {
+    calHtml += `<button class = "upBtn">${calValue}</button>`
+  } else {
+  calHtml += `<button class = "valueBtn">${calValue}</button>`
+  }
   document.querySelector('.cal-btn').innerHTML = calHtml
 })
